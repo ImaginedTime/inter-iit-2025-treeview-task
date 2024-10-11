dbe5c9f2313e4243e422ccf3c697093310f8ece2
ImaginedTime <udayomsrivastava@kgpian.iitkgp.ac.in>
2024-10-11 18:32:17 +0530

    navbar removal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
 import { Poppins } from "next/font/google";
 import { UserProvider } from "@/context/UserContext";
 import { Toaster } from "react-hot-toast";
